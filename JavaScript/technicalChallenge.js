92cb6687cf54e7df9f23fb2ade5da8833cb327cf
AtlasRB <connoryeeles@outlook.com>
2024-11-01 12:30:07 +0000

    TASK 3: validates that the text field contains an integer when the form is submitted, if not it displays an error

--- JavaScript/technicalChallenge.js
+++ JavaScript/technicalChallenge.js
@@ -6,4 +6,9 @@ document.getElementById("getProductStatusID").addEventListener("submit", functio
         console.log("ERROR: Please enter a Product Status ID before submitting."); //Creates an error line in the webpage console
         alert("Please enter a Product Status ID before submitting."); //Creates an error alert popup on the webpage
     }
+    if (!Number.isInteger(Number(productStatusInput))) { //NOTE: Done as a separate if statement instead of as an or in the above if, so that the error message can be more unique.
+        event.preventDefault();
+        console.log("ERROR: Please enter a full number value for Product Status ID before submitting."); //Creates an error line in the webpage console
+        alert("Please enter a full number value for Product Status ID before submitting."); //Creates an error alert popup on the webpage
+    }
 });
